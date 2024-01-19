6884d7324255060e81b4008de64ab7997af5219f
Nikhil Sharma <nikhil.sharma.sp@lhsystems.com>
2024-01-19 10:52:47 +0530

    push everything

---
+++ math/src/main/java/com/gfg/mustdo/App.java
@@ -0,0 +1,18 @@
+package com.gfg.mustdo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+    public String execute() {
+        return "Hello World";
+    }
+
+    public static void main( String[] args )
+    {
+         System.out.println(new App().execute());
+    }
+}
---
+++ math/src/test/java/com/gfg/mustdo/AppTest.java
@@ -0,0 +1,24 @@
+package com.gfg.mustdo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        App a = new App();
+        assertTrue("Hello World".equals(a.execute()));
+        assertTrue( true );
+    }
+}
